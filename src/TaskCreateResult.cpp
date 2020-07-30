4182b4396146055b19a2d3283a5eca44ce7c9799
Vokadus <44712357+Vokadus@users.noreply.github.com>
2020-07-31 00:57:50 +0300

    Removed TaskCreateResult

--- src/TaskCreateResult.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by R2D2 on 30.07.2020.
-//
-
-#include "TaskCreateResult.h"
-TaskCreateResult::TaskCreateResult(TaskCreateResult::Result result,
-                                   const std::optional<Task> &task,
-                                   const std::optional<std::string> &message)
-    : result_(result), task_(task), message_(message) {}
