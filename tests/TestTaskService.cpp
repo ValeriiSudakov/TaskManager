23637d42ff17889582e6b6952020b9248135ca2d
Vokadus <44712357+Vokadus@users.noreply.github.com>
2020-08-03 17:10:47 +0300

    Added new tests for TaskService

--- tests/TestTaskService.cpp
+++ tests/TestTaskService.cpp
@@ -45,6 +45,23 @@ TEST_F(TestTaskServiceClass, shouldntCreateSubTaskWithIncorrectID) {
   ASSERT_FALSE(ts.AddSubtask(incorrectID, subTask.value(), Task::Priority::NONE));
 }
 
+
+TEST_F(TestTaskServiceClass, shouldFoundTaskByName) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTasksByName("task", false);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundTaskByNameAndPriority) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTasksByName("task", true);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
 TEST_F(TestTaskServiceClass, shouldNotFoundTaskByName) {
   TaskService ts;
   std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
@@ -53,10 +70,108 @@ TEST_F(TestTaskServiceClass, shouldNotFoundTaskByName) {
   ASSERT_TRUE(resultSearch.empty());
 }
 
-TEST_F(TestTaskServiceClass, shouldFoundTaskByName) {
+TEST_F(TestTaskServiceClass, shouldFoundTaskByLabel) {
   TaskService ts;
   std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
   ts.AddTask(task.value(), Task::Priority::NONE);
-  auto resultSearch = ts.GetTasksByName("asf125safs", false);
+  auto resultSearch = ts.GetTasksByLabel("label", false);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundTaskByLabelAndPriority) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTasksByLabel("label", true);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldNotFoundTaskByLabel) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTasksByLabel("124rwfsaa21", false);
+  ASSERT_TRUE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundTasks) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTasks( false);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundTasksByPriority) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTasks(true);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldNotFoundTasks) {
+  TaskService ts;
+  auto resultSearch = ts.GetTasks(false);
+  ASSERT_TRUE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundTodayTasks) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTodayTasks( false);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundTodayTasksByPriority) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTodayTasks(true);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldNotFoundTodayTasks) {
+  TaskService ts;
+  auto resultSearch = ts.GetTodayTasks(false);
+  ASSERT_TRUE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundWeekTasks) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetWeekTasks( false);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundWeekTasksByPriority) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetWeekTasks(true);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldNotFoundWeekTasks) {
+  TaskService ts;
+  auto resultSearch = ts.GetWeekTasks(false);
+  ASSERT_TRUE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldFoundTasksPriority) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTasksByPriority(Task::Priority::NONE);
+  ASSERT_FALSE(resultSearch.empty());
+}
+
+TEST_F(TestTaskServiceClass, shouldNotFoundTasksPriority) {
+  TaskService ts;
+  std::optional<Task> task = Task::Create("task", "label", Task::Priority::NONE, Date::GetCurrentTime());
+  ts.AddTask(task.value(), Task::Priority::NONE);
+  auto resultSearch = ts.GetTasksByPriority(Task::Priority::FIRST);
   ASSERT_TRUE(resultSearch.empty());
 }
