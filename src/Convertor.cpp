e8e59928318db91e184355606e06ca59fc745af3
valeriisudakov <valerii.sudakov@teamdev.com>
2020-08-17 15:34:15 +0300

    Added tests for Convertor and improve sortedVectorByPriority function

--- src/Convertor.cpp
+++ src/Convertor.cpp
@@ -16,17 +16,12 @@ std::vector<TaskDTO> convertor::toTaskDTO::notSortedVector(const std::vector<Tas
 }
 
 std::vector<TaskDTO>  convertor::toTaskDTO::sortedVectorByPriority(const std::vector<TaskEntity>& tasksForDTO){
-  std::multimap<Priority, TaskEntity> sortedStore;
-  for (const auto& task : tasksForDTO){
-    sortedStore.insert(std::make_pair(task.GetPriority(), task));
-  }
-
   std::vector<TaskDTO> tasksDTO;
-  for (const auto& task : sortedStore){
-    auto dto = TaskDTO::Create(task.second.GetName(), task.second.GetLabel(), task.second.GetPriority(),
-                               task.second.GetDueDate(), task.second.IsComplete(), task.second.GetId());
+  for (const auto& task : tasksForDTO){
+    auto dto = TaskDTO::Create(task.GetName(), task.GetLabel(), task.GetPriority(),
+                               task.GetDueDate(), task.IsComplete(), task.GetId());
     tasksDTO.push_back(dto);
   }
-
+  std::sort(tasksDTO.begin(), tasksDTO.end(), [](TaskDTO a, TaskDTO b) { return a.GetPriority() < b.GetPriority(); });
   return tasksDTO;
 }
