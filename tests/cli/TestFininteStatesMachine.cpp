9a80f76d2512961a2dd710b93e07c528ca1e88ed
valeriisudakov <valerii.sudakov@teamdev.com>
2020-09-14 11:36:30 +0300

    Added tests for FiniteStatesMachine and InputTaskParams

---
+++ tests/cli/TestFininteStatesMachine.cpp
@@ -0,0 +1,66 @@
+//
+// Created by valeriisudakov on 14.09.20.
+//
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "Factory/Factory.h"
+#include <memory>
+#include "InputOutputLayer.h"
+
+using ::testing::Return;
+
+
+class TestFiniteStatesMachine :  public ::testing::Test {
+
+};
+
+class MockIO : public InputOutputLayer{
+ public:
+  MOCK_METHOD(std::string, Input, (), (override));
+  MOCK_METHOD(void, Output, (const std::string&), (override));
+};
+
+
+
+TEST_F(TestFiniteStatesMachine, shouldCorrectChangeStatesFiniteStatesMachine){
+
+  auto context = std::make_shared<Context>(nullptr);
+  auto io = std::make_shared<MockIO>();
+
+  // 5 times - "input smth" , 1 time  - "error"
+  EXPECT_CALL(*io, Output).Times(6).WillRepeatedly(Return());
+  EXPECT_CALL(*io, Input).Times(5).WillOnce(Return("name"))
+                                       .WillOnce(Return(""))
+                                       .WillOnce(Return("Label"))
+                                       .WillOnce(Return("first"))
+                                       .WillOnce(Return("now"));
+
+  auto stateMachine = Factory::CreateFiniteStatesMachine(FiniteStateMachineID::INPUT_TASK_PARAMS,
+                                                         context,
+                                                         io);
+  stateMachine->Execute();
+
+
+}
+
+
+TEST_F(TestFiniteStatesMachine, shouldCorrectChangeStatesMenuStatesMachine) {
+
+  auto context = std::make_shared<Context>(nullptr);
+  auto io = std::make_shared<MockIO>();
+
+  // menus output: 8 +  "input command" * "name, label, priority, date, success" +8
+  EXPECT_CALL(*io, Output).Times(22).WillRepeatedly(Return());
+  EXPECT_CALL(*io, Input).Times(6).WillOnce(Return("add task"))
+                                      .WillOnce(Return("name"))
+                                      .WillOnce(Return("label"))
+                                      .WillOnce(Return("first"))
+                                      .WillOnce(Return("now"))
+                                      .WillOnce((Return("exit")));
+
+  auto inputNameMachine = Factory::CreateMenuStateMachine(StatesID::BASE_MENU,
+                                                        context,
+                                                        io);
+  inputNameMachine->Execute();
+}
---
+++ tests/cli/TestInputTaskParams.cpp
@@ -0,0 +1,63 @@
+//
+// Created by valeriisudakov on 14.09.20.
+//
+
+
+#include "Factory/Factory.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <memory>
+#include "InputOutputLayer.h"
+
+
+class TestInput :  public ::testing::Test {
+
+};
+
+using ::testing::Return;
+
+class MockIO : public InputOutputLayer{
+ public:
+  MOCK_METHOD(std::string, Input, (), (override));
+  MOCK_METHOD(void, Output, (const std::string&), (override));
+};
+
+
+TEST_F(TestInput, shouldCorrectInputName){
+  auto io = std::make_shared<MockIO>();
+  EXPECT_CALL(*io, Output).Times(3).WillRepeatedly(Return());
+  EXPECT_CALL(*io, Input).Times(2).WillOnce(Return(""))
+                                      .WillOnce(Return("name"));
+
+  auto context = std::make_shared<Context>(nullptr);
+  auto name = Factory::CreateFiniteStatesMachine(FiniteStateMachineID::INPUT_NAME,
+                                                 context,
+                                                 io);
+  name->Execute();
+}
+
+TEST_F(TestInput, shouldCorrectInputLabel){
+  auto io = std::make_shared<MockIO>();
+  EXPECT_CALL(*io, Output).Times(3).WillRepeatedly(Return());
+  EXPECT_CALL(*io, Input).Times(2).WillOnce(Return(""))
+      .WillOnce(Return("label"));
+
+  auto context = std::make_shared<Context>(nullptr);
+  auto label = Factory::CreateFiniteStatesMachine(FiniteStateMachineID::INPUT_LABEL,
+                                                 context,
+                                                 io);
+  label->Execute();
+}
+
+
+TEST_F(TestInput, shouldCorrectInputDate){
+  auto io = std::make_shared<MockIO>();
+  EXPECT_CALL(*io, Output).Times(1).WillRepeatedly(Return());
+  EXPECT_CALL(*io, Input).Times(1).WillOnce(Return("now"));
+
+  auto context = std::make_shared<Context>(nullptr);
+  auto date = Factory::CreateFiniteStatesMachine(FiniteStateMachineID::INPUT_DATE,
+                                                  context,
+                                                  io);
+  date->Execute();
+}
