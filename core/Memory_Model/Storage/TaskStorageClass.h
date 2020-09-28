bb80e3e19b6a5d067af0388017075c0727fa7484
valeriisudakov <valerii.sudakov@teamdev.com>
2020-09-28 15:40:13 +0300

    Removed AddSubtask method

--- core/Memory_Model/Storage/TaskStorageClass.h
+++ core/Memory_Model/Storage/TaskStorageClass.h
@@ -14,12 +14,11 @@ class TaskStorageClass : public TaskStorage{
  public:
   const std::map<TaskID, std::shared_ptr<TaskEntity>>&    GetTasks() const override;
   std::optional<std::shared_ptr<TaskEntity>>              GetTask(const TaskID& taskID) const override;
-  std::optional<std::shared_ptr<TaskEntity>>              AddTask(const Task& task) override;
-  std::optional<std::shared_ptr<TaskEntity>>              AddSubtask(const TaskID& rootTaskID, const Task& subtask) override;
+  std::optional<std::shared_ptr<TaskEntity>>              AddTask(const TaskEntity& task) override;
   bool                                                    RemoveTask(const TaskID& id) override;
   bool                                                    PostponeTask(const TaskID& id, const Date& date) override;
+
  private:
-  TaskIDGenerate                                          taskIDGenerate_;
   std::map<TaskID, std::shared_ptr<TaskEntity>>           tasks_;
 };
 
