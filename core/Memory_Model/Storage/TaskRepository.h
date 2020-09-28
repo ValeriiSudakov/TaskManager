e2e534027045635d7625bc558f31504faf717e28
valeriisudakov <valerii.sudakov@teamdev.com>
2020-09-28 15:37:34 +0300

    Now TaskRepository input and output tasks are TaskRepositoryDTO; Now TaskRepository is a isolated module; Moved TaskIDGenerator from Storage to here. Also New Task Created here and moving to View and Storage. Added Sort and Converts private methods

--- core/Memory_Model/Storage/TaskRepository.h
+++ core/Memory_Model/Storage/TaskRepository.h
@@ -5,24 +5,30 @@
 #ifndef TASKMANAGER_SRC_MEMORY_MODEL_STORAGE_TASKREPOSITORYINTERFACE_H_
 #define TASKMANAGER_SRC_MEMORY_MODEL_STORAGE_TASKREPOSITORYINTERFACE_H_
 #include "API/AddTaskResult.h"
-#include "API/TaskServiceDTO.h"
+#include "TaskRepositoryDTO.h"
 #include "API/Priority.h"
-#include "TaskViewClass.h"
-#include "TaskStorageClass.h"
 
 class TaskRepository{
  public:
   virtual ~TaskRepository() = default;
 
  public:
-  virtual void                                              ResetRepository() = 0;
-  virtual const std::unique_ptr<TaskView>&                  GetTaskView() const = 0;
-  virtual const std::unique_ptr<TaskStorage>&               GetTaskStorage() const = 0;
+  virtual void                                ResetRepository() = 0;
 
-  virtual AddTaskResult                                     AddTask(const Task& task) = 0;
-  virtual AddTaskResult                                     AddSubtask(const TaskID& rootTaskID, const Task& subtask) = 0;
-  virtual bool                                              RemoveTask(const TaskID& id) = 0;
-  virtual bool                                              PostponeTask(const TaskID& id, const Date& date) = 0;
+  virtual AddTaskResult                       AddTask(const TaskRepositoryDTO& task) = 0;
+  virtual AddTaskResult                       AddSubtask(const TaskID& rootTaskID, const TaskRepositoryDTO& subtask) = 0;
+  virtual bool                                RemoveTask(const TaskID& id) = 0;
+  virtual bool                                PostponeTask(const TaskID& id, const Date& date) = 0;
+  virtual bool                                SetTaskComplete(const TaskID& ID) = 0;
+
+  virtual std::optional<TaskRepositoryDTO>    GetTask(const TaskID& id) const = 0;
+  virtual std::vector<TaskRepositoryDTO>      GetSubtask(const TaskID& id) const = 0;
+  virtual std::vector<TaskRepositoryDTO>      GetTasks(bool byPriority) const = 0;
+  virtual std::vector<TaskRepositoryDTO>      GetTodayTasks(bool byPriority) const = 0;
+  virtual std::vector<TaskRepositoryDTO>      GetWeekTasks(bool byPriority) const = 0;
+  virtual std::vector<TaskRepositoryDTO>      GetTasksByLabel(const std::string& label) const = 0;
+  virtual std::vector<TaskRepositoryDTO>      GetTasksByName(const std::string& name) const = 0;
+  virtual std::vector<TaskRepositoryDTO>      GetTasksByPriority(const Priority& priority) const = 0;
 };
 
 #endif //TASKMANAGER_SRC_MEMORY_MODEL_STORAGE_TASKREPOSITORYINTERFACE_H_
