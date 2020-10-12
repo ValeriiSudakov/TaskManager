e5b9854224f09dddd5f2c11e88fb762f25de64aa
valeriisudakov <valerii.sudakov@teamdev.com>
2020-10-12 21:21:41 +0300

    Added multithreading to save/loads to core

--- core/Memory_Model/Storage/TaskRepositoryController.cpp
+++ core/Memory_Model/Storage/TaskRepositoryController.cpp
@@ -5,6 +5,7 @@
 #include "TaskRepositoryController.h"
 #include "Persister/TaskPersister.h"
 #include "Persister/TaskPersisterUtils.h"
+#include <future>
 
 const std::unique_ptr<TaskRepository>& TaskRepositoryController::Get() const {
   return tasksRepository_;
@@ -13,20 +14,26 @@ const std::unique_ptr<TaskRepository>& TaskRepositoryController::Get() const {
 bool TaskRepositoryController::Save(const std::string& filename) {
   std::fstream file(filename, std::ios::out);
   auto persister = PersisterUtils::Create(*tasksRepository_, file);
-  auto result = persister->Save();
+
+  auto save = std::async(std::bind(&Persister::Save, persister.get()));
+  save.wait();
+
   file.close();
-  return result;
+  return save.get();
 }
 
 bool TaskRepositoryController::Load(const std::string& filename) {
   std::fstream file(filename, std::ios::in);
   auto newRepository = std::move(repositoryFactory_->Create());
   auto persister = PersisterUtils::Create(*newRepository, file);
-  auto result = persister->Load();
 
-  if (result){
+  auto load = std::async(std::bind(&Persister::Load, persister.get()));
+  load.wait();
+
+  bool loadSuccess = load.get();
+  if (loadSuccess){
     std::swap(tasksRepository_, newRepository);
   }
   file.close();
-  return result;
+  return loadSuccess;
 }
