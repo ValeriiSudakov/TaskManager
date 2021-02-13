4a1276f14043c2e2b7fbd9164a5138c99ae2f996
Valerii Sudakov <valerii.sudakov@teamdev.com>
2021-02-13 17:05:23 +0200

    Made src directory in root folder. Moved core, cli, client and server there

--- cmake/project.cmake
+++ cmake/project.cmake
@@ -1,19 +1,19 @@
-include_directories(core)
-include_directories(cli)
+include_directories(${SRC_ROOT}/cli)
+include_directories(${SRC_ROOT}/core)
 
 ############## cli ########################################################
-file(GLOB_RECURSE cli_SOURCES "cli/*.cpp")
-file(GLOB_RECURSE cli_HEADERS "cli/*.h")
+file(GLOB_RECURSE cli_SOURCES "${SRC_ROOT}/cli/*.cpp")
+file(GLOB_RECURSE cli_HEADERS "${SRC_ROOT}/cli/*.h")
 ############## core ########################################################
-file(GLOB_RECURSE core_SOURCES "core/*.cpp")
-file(GLOB_RECURSE core_HEADERS "core/*.h")
+file(GLOB_RECURSE core_SOURCES "${SRC_ROOT}/core/*.cpp")
+file(GLOB_RECURSE core_HEADERS "${SRC_ROOT/}core/*.h")
 
 ############ Server #####################################################
-add_executable(Server Server.cpp ${cli_SOURCES} ${core_SOURCES} ${PROTO_SRCS} ${service_proto_srcs} ${service_grpc_srcs})
+add_executable(Server ${SRC_ROOT}/Server.cpp ${cli_SOURCES} ${core_SOURCES} ${PROTO_SRCS} ${service_proto_srcs} ${service_grpc_srcs})
 target_include_directories(Server PRIVATE ${cli_HEADERS} ${core_HEADERS} ${PROTO_HDRS})
 target_link_libraries(Server gRPC::grpc++ gRPC::grpc++_reflection ${Protobuf_LIBRARIES} proto_files)
 
 ############ Client #####################################################
-add_executable(Client Client.cpp ${cli_SOURCES} ${core_SOURCES} ${PROTO_SRCS} ${service_proto_srcs} ${service_grpc_srcs})
+add_executable(Client ${SRC_ROOT}/Client.cpp ${cli_SOURCES} ${core_SOURCES} ${PROTO_SRCS} ${service_proto_srcs} ${service_grpc_srcs})
 target_include_directories(Client PRIVATE ${cli_HEADERS} ${core_HEADERS} ${PROTO_HDRS})
 target_link_libraries(Client gRPC::grpc++ gRPC::grpc++_reflection ${Protobuf_LIBRARIES} proto_files)
