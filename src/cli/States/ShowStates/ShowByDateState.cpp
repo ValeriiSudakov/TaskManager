1c42da83cb2b4e23a8425850fe124d77dd224a07
vsudakov <vsudakov@cisco.com>
2021-06-06 17:38:48 +0300

    Created ShowByDateState class

---
+++ src/cli/States/ShowStates/ShowByDateState.cpp
@@ -0,0 +1,48 @@
+/*!
+* Copyright (c) 2021 by Cisco Systems, Inc.
+* All rights reserved.
+*/
+//
+
+#include "ShowByDateState.h"
+#include "InputOutputConsoleLayer.h"
+#include "Factory.h"
+
+ShowByDateState::ShowByDateState() : State(StatesID::SHOW_THIS_WEEK) {}
+
+ShowByDateState::~ShowByDateState() = default;
+
+StateOperationResult ShowByDateState::Do(const std::shared_ptr<Context>& context, InputOutputLayer& io) {
+
+  auto inputIDStateMachine = Factory::CreateFiniteStatesMachine( FiniteStateMachineID::INPUT_DATE,
+                                                                 context,
+                                                                 io);
+  inputIDStateMachine->Execute();
+
+  std::string output { "Tasks list will be updated. Sort tasks by priority? [y/n]: " };
+  io.Output(output);
+  std::string inputSort {io.Input()};
+
+  if (inputSort == "y") {
+    context->tasks_ = context->taskService_.GetTasksByDate(context->buffer_.date, true);
+  } else if (inputSort == "n") {
+    context->tasks_ = context->taskService_.GetTasksByDate(context->buffer_.date, false);
+  } else {
+    std::string error { "Incorrect input.\n" };
+    io.Output(error);
+    return StateOperationResult::INCORRECT_INPUT;
+  }
+  if (context->tasks_.empty()){
+    std::string notFound { "Tasks were not found.\n" };
+    io.Output(notFound);
+    return StateOperationResult::TASKS_LIST_EMPTY;
+  }
+  int taskNumber = 0;
+  io.Output(std::string{"\n"});
+  for (const auto& task : context->tasks_){
+    std::string taskStr { std::to_string(taskNumber++) + ": " + task.GetName() + "\n" };
+    io.Output(taskStr);
+  }
+
+  return StateOperationResult::SUCCESS;
+}
